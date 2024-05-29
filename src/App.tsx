28d40f20fa522eab67474eb7cf0c7985b20a3d79
Ekaterina Bondareva <1978231@bk.ru>
2024-05-29 21:31:01 +0500

    routes fot authorized users and others

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import { AuthorizationContext } from "./AuthorizationContextProvider";
 import { useContext } from "react";
 import { RoutesObject } from "./Routes";
 import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const App: React.FC = () => {
   const { isLogedIn } = useContext(AuthorizationContext);
-
   return isLogedIn ? (
-    <Route path={RoutesObject.mainPage} element={<MainPage />}></Route>
+    <Routes>
+      <Route path={RoutesObject.mainPage} element={<MainPage />}></Route>
+    </Routes>
   ) : (
     <Routes>
       <Route path={RoutesObject.home} element={<SignInSide />}></Route>
+      <Route
+        path={"*"}
+        element={<Navigate to={RoutesObject.home} replace />}
+      ></Route>
     </Routes>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { AuthorizationContextProvider } from "./AuthorizationContextProvider";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <AuthorizationContextProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AuthorizationContextProvider>
   </React.StrictMode>
 );
