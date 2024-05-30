a2ea01e3f42be4fde5d8e448e6229a5da69399d9
Ekaterina Bondareva <1978231@bk.ru>
2024-05-30 15:25:09 +0500

    changed sign in page design

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -136,10 +136,6 @@ export function SignInSide() {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
               <Button
                 type="submit"
                 fullWidth
